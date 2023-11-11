0cc2c57cab0d4b64c8a0f2193acbf5bea4ffed26
Junho Lee <ghzm888@gmail.com>
2023-11-12 01:19:01 +0900

    feat : Enum 타입 할인 가격들 작성

---
+++ src/main/java/christmas/global/utils/constant/DiscountType.java
@@ -0,0 +1,23 @@
+package christmas.global.utils.constant;
+
+public enum DiscountType {
+
+    ZERO(0),
+    MAX_COUNT_OF_MENU(20),
+    AMOUNT_OF_FIRST_CHRISTMAS_DISCOUNT(1000),
+    AMOUNT_OF_PLUS_CHRISTMAS_DISCOUNT(100),
+    AMOUNT_OF_WEEKDAY_DISCOUNT(2023),
+    AMOUNT_OF_WEEKEND_DISCOUNT(2023),
+    AMOUNT_OF_SPECIAL_DISCOUNT(1000),
+    MIN_AMOUNT_OF_ORDER(10000),
+    MIN_AMOUNT_OF_EVENT(120000);
+    private final int value;
+
+    DiscountType(final int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
