3d2a205fdcaac8c373ea5dc23fe016983e27a03b
Junho Lee <ghzm888@gmail.com>
2023-11-14 11:13:08 +0900

    refactor : 서비스와 컨트롤러 받아와 작동하도록 구현

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,7 +1,18 @@
 package christmas;
 
+import christmas.domain.controller.ChristmasController;
+import christmas.domain.service.BadgeService;
+import christmas.domain.service.ClientService;
+import christmas.domain.service.DiscountService;
+import christmas.domain.service.MenuService;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        ClientService clientService = new ClientService();
+        MenuService menuService = new MenuService();
+        DiscountService discountService = new DiscountService();
+        BadgeService badgeService = new BadgeService();
+        ChristmasController christmasController = new ChristmasController(clientService, menuService, discountService, badgeService);
+        christmasController.run();
     }
 }
