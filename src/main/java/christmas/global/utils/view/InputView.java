de174856b7131e9f378b4344f28aaa18d4dd99f9
Junho Lee <ghzm888@gmail.com>
2023-11-14 11:04:31 +0900

    feat : Console 입력받는 InputView 작성

---
+++ src/main/java/christmas/global/utils/view/InputView.java
@@ -0,0 +1,13 @@
+package christmas.global.utils.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    private InputView() {
+    }
+
+    public static String input() {
+        return Console.readLine();
+    }
+}
