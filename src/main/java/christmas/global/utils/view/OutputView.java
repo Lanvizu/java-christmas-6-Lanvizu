5497a6a30e33d120e0bd6aa1a95833dfe0bb7371
Junho Lee <ghzm888@gmail.com>
2023-11-14 11:05:07 +0900

    feat : Console 로 출력하는 OutputView 작성

---
+++ src/main/java/christmas/global/utils/view/OutputView.java
@@ -0,0 +1,11 @@
+package christmas.global.utils.view;
+
+public class OutputView {
+
+    private OutputView() {
+    }
+
+    public static void commonOutputLine(String output) {
+        System.out.println(output);
+    }
+}
